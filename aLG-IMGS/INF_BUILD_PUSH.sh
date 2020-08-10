e16ff30c3d096a431be97e9a3fde8fc9a0b00f86
Viswanath Chadalapaka <viswanath.chadalapaka@gmail.com>
2020-08-09 23:11:18 -0700

    better push scripts

--- aLG-IMGS/INF_BUILD_PUSH.sh
+++ aLG-IMGS/INF_BUILD_PUSH.sh
@@ -1,7 +1,8 @@
 tag=${1}
-aws ecr get-login-password --region us-east-1 | docker login --username AWS --password-stdin 651826702434.dkr.ecr.us-east-1.amazonaws.com
+reg=${2}
+aws ecr get-login-password --region us-east-1 | docker login --username AWS --password-stdin ${reg}.dkr.ecr.us-east-1.amazonaws.com
 docker build -t multimodal-inference -f inference-container/Dockerfile.inference .
-docker tag multimodal-inference:latest 651826702434.dkr.ecr.us-east-1.amazonaws.com/multimodal-inference:${tag}
-docker push 651826702434.dkr.ecr.us-east-1.amazonaws.com/multimodal-inference:${tag}
+docker tag multimodal-inference:latest ${reg}.dkr.ecr.us-east-1.amazonaws.com/multimodal-inference:${tag}
+docker push ${reg}.dkr.ecr.us-east-1.amazonaws.com/multimodal-inference:${tag}
 docker rmi multimodal-inference
 docker image prune -f
