80765ff14412a6a7e73c1671767d3c4c7bc798eb
Viswanath Chadalapaka <viswanath.chadalapaka@gmail.com>
2020-08-10 15:28:32 +0000

    +inferencer bug fix

--- aLG-IMGS/inference-container/model_handler.py
+++ aLG-IMGS/inference-container/model_handler.py
@@ -387,7 +387,7 @@ class ModelHandler(object):
             wholistic_test = pd.concat([wholistic_test, pd.DataFrame(preds_cat)], axis=1)
         if has_img:
             for i in range(len(preds_img)):
-                wholistic_test = pd.concat([wholistic_test, pd.DataFrame(preds_img)], axis=1)
+                wholistic_test = pd.concat([wholistic_test, pd.DataFrame(preds_img[i])], axis=1)
         wholistic_input_size = wholistic_test.shape[1]
 
         print('predicting...')
